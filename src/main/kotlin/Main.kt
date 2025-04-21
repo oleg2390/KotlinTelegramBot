1e7fa422cf2d841bd3602d087e58e1ae7b82a33d
oleg <ip.pan@bk.ru>
2025-04-21 19:59:15 +0400

    KTB4-04-tb4 create data class and add list and print

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,12 +2,33 @@ package org.example
 
 import java.io.File
 
+data class Word(
+    val original: String,
+    val translate: String,
+    var correctAnswersCount: Int = 0,
+)
+
 fun main() {
 
     val wordsFile: File = File("words.txt")
+    var dictionary = mutableListOf<Word>()
 
     val lines = wordsFile.readLines()
     for (line in lines) {
-        println(line)
+        val line = line.split("|")
+        val correctCount = line.getOrNull(2) ?: 0
+        val word = Word(original = line[0], translate = line[1])
+        dictionary.add(word)
+        println(word)
+    }
+    dictionary.forEach { println("${it.original} - ${it.translate} - ${it.correctAnswersCount}") }
+}
+
+class Word1(
+    val text: String,
+    val translate: String,
+) {
+    override fun toString(): String {
+        return "$text - $translate"
     }
 }
