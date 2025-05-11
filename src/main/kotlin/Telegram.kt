1ddf6a9307f0761036060b76370ff304091ec4ad
oleg <ip.pan@bk.ru>
2025-05-11 22:47:03 +0400

    KTB-15-tb15 refactor

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -3,6 +3,8 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
+const val HTTP_URL = "https://api.telegram.org/bot"
+
 fun main(args: Array<String>) {
 
     val botToken = args[0]
@@ -11,8 +13,8 @@ fun main(args: Array<String>) {
 
     while (true) {
         Thread.sleep(2000)
-        val telegramBotService = TelegramBotService()
-        val updates: String = telegramBotService.getUpdates(botToken, updateId)
+        val telegramBotService = TelegramBotService(botToken)
+        val updates: String = telegramBotService.getUpdates(updateId)
         println(updates)
 
         val matches = updateIdRegex.findAll(updates)
@@ -30,37 +32,36 @@ fun main(args: Array<String>) {
         val text = matchResult.lastOrNull()?.groups?.get(1)?.value.toString()
         println(text)
 
-        val sendRegexChat: Regex = "\"chat\":\\{\"id\":(-?\\d+)".toRegex()
-        val matchResultChat = sendRegexChat.find(updates)
-        val groupsChat = matchResultChat?.groups
-        val chatIdResult = groupsChat?.get(1)?.value?.toLong()
+        val chatIdResult: Long = "\"chat\":\\{\"id\":(-?\\d+)".toRegex()
+            .find(updates)
+            ?.groups?.get(1)?.value?.toLongOrNull()
+            ?: throw IllegalArgumentException("Ошибка, chat_id не найден")
 
-        telegramBotService.sendMessage(botToken, text, chatIdResult)
+        telegramBotService.sendMessage(text, chatIdResult)
     }
 }
 
-class TelegramBotService {
+class TelegramBotService(
+    private val token: String,
+) {
 
-    fun getUpdates(token: String, updateId: Int): String {
-        val urlGetUpdates = "https://api.telegram.org/bot$token/getUpdates?offset=$updateId"
+    fun getUpdates(updateId: Int): String {
+        val urlGetUpdates = "$HTTP_URL$token/getUpdates?offset=$updateId"
         val client: HttpClient = HttpClient.newBuilder().build()
         val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
         val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
         return response.body()
     }
 
-    fun sendMessage(token: String, text: String, chatId: Long?): String {
+    fun sendMessage(text: String, chatId: Long): String {
 
-        when {
-            text.isBlank() -> return "Ошибка, текст не может быть пустым"
-            chatId == null -> return "Ошибка, chatId не указан"
-        }
+        if (text.isBlank()) return "Ошибка, текст не может быть пустым"
 
         val limitsText = if (text.length > 4096) {
             text.substring(0, 4096)
         } else text
 
-        val urlGetChat = "https://api.telegram.org/bot$token/sendMessage"
+        val urlGetChat = "$HTTP_URL$token/sendMessage"
         val clientChat = HttpClient.newBuilder().build()
 
         val jsonBody = """
