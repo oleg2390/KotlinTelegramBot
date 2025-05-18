7e421630562d5c5341c18d2171291ec74a0072b6
oleg <ip.pan@bk.ru>
2025-05-18 10:48:43 +0400

    KTB-19-tb19 refactor

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -1,6 +1,7 @@
 import org.example.LearnWordsTrainer
 
 const val HTTP_URL = "https://api.telegram.org/bot"
+const val START_COMMAND = "/start"
 
 fun main(args: Array<String>) {
 
@@ -11,6 +12,7 @@ fun main(args: Array<String>) {
     val updateIdRegex: Regex = "\"update_id\":(\\d+)".toRegex()
     val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
     val dataRegex: Regex = "\"data\":\"(.+?)\"".toRegex()
+    val chatIdRegex: Regex = "\"chat\":\\{\"id\":(\\d+)".toRegex()
 
     while (true) {
         Thread.sleep(2000)
@@ -25,14 +27,11 @@ fun main(args: Array<String>) {
         val matchResult = messageTextRegex.findAll(updates).toList()
         val text = matchResult.lastOrNull()?.groups?.get(1)?.value.toString()
         val data = dataRegex.find(updates)?.groups?.get(1)?.value
-
-        val chatIdResult: Long = "\"chat\":\\{\"id\":(\\d+)".toRegex()
-            .find(updates)
-            ?.groups?.get(1)?.value?.toLongOrNull()
-            ?: throw IllegalArgumentException("Ошибка, chat_id не найден")
+        val chatIdResult: Long = chatIdRegex
+            .find(updates)?.groups?.get(1)?.value?.toLongOrNull() ?: continue
 
         when {
-            text.lowercase() == "/start" -> telegramBotService.sendMenu(chatIdResult)
+            text.lowercase() == START_COMMAND -> telegramBotService.sendMenu(chatIdResult)
             data?.lowercase() == STATISTICS_CLICKED -> {
                 val infoStatistics = trainer.getStatistics()
                 telegramBotService
